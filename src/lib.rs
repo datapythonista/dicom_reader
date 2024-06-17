95d5b8788c635a951b6ab1943ccf788fceaf2669
Marc Garcia <garcia.marc@gmail.com>
2024-06-18 00:52:29 +0400

    Adding read_dicom to Polars

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,34 @@ use arrow::pyarrow::ToPyArrow;
 mod reader;
 mod data;
 
-#[pyfunction]
-fn read_dicom(path: &str) -> PyResult<Py<PyAny>> {
+fn read_dicom(path: &str) -> Py<PyAny> {
     let dicom_reader = reader::DicomReader::new(path);
     let record_batch = data::create_record_batch(dicom_reader);
     Python::with_gil(|py| {
-        let pyarrow_record_batch = record_batch.to_pyarrow(py).unwrap();
+        record_batch.to_pyarrow(py).unwrap()
+    })
+}
+
+#[pyfunction]
+#[pyo3(name="read_dicom")]
+fn read_dicom_to_pandas(path: &str) -> PyResult<Py<PyAny>> {
+    let pyarrow_record_batch = read_dicom(path);
+    Python::with_gil(|py| {
         let pandas_dataframe = pyarrow_record_batch.call_method0(py, "to_pandas").unwrap();
         Ok(pandas_dataframe)
+    })
+}
+
+#[pyfunction]
+#[pyo3(name="read_dicom")]
+fn read_dicom_to_polars(path: &str) -> PyResult<Py<PyAny>> {
+    let pyarrow_record_batch = read_dicom(path);
+    Python::with_gil(|py| {
+        let polars = Python::import_bound(py, "polars").unwrap();
+        let from_arrow = polars.getattr("from_arrow").unwrap();
+        let args = (pyarrow_record_batch,);
+        let polars_dataframe = from_arrow.call1(args).unwrap().into();
+        Ok(polars_dataframe)
     })
 }
 
@@ -18,7 +38,11 @@ fn read_dicom(path: &str) -> PyResult<Py<PyAny>> {
 fn dicom_reader(_module: &Bound<'_, PyModule>) -> PyResult<()> {
     Python::with_gil(|py| {
         if let Ok(pandas) = Python::import_bound(py, "pandas") {
-            pandas.add_function(wrap_pyfunction!(read_dicom, &pandas).unwrap()).unwrap();
+            pandas.add_function(wrap_pyfunction!(read_dicom_to_pandas, &pandas).unwrap()).unwrap();
+        }
+
+        if let Ok(polars) = Python::import_bound(py, "polars") {
+            polars.add_function(wrap_pyfunction!(read_dicom_to_polars, &polars).unwrap()).unwrap();
         }
     });
     Ok(())
